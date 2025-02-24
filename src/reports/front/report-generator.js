bcb6df6c05c3cc89bb088c6cdda78d786d577125
Danilo Vezzoni <underc0de.org@gmail.com>
2025-02-24 01:50:34 -0300

    Update cucumber report

---
+++ src/reports/front/report-generator.js
@@ -0,0 +1,25 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var report = require("multiple-cucumber-html-reporter");
+report.generate({
+    jsonDir: "src/reports/front", // Directorio donde está el archivo cucumber-report.json
+    reportPath: "src/reports/front", // Carpeta donde se generará el reporte HTML
+    metadata: {
+        browser: {
+            name: "chrome",
+            version: "latest"
+        },
+        device: "Local test machine",
+        platform: {
+            name: "Windows",
+            version: "10"
+        }
+    },
+    customData: {
+        title: "Test Execution Report",
+        data: [
+            { label: "Project", value: "K0lmena" },
+            { label: "Execution Date", value: new Date().toLocaleString() }
+        ]
+    }
+});
